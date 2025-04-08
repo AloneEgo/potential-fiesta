9259ab967ecd27ac0ec83a633b5f7eda96f2aa70
Oleg Yakovenko <sc0rpi0n2001@yandex.com>
2025-04-08 22:57:44 +0300

    Сделал ДЗ

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,2 +1,9 @@
 package ru.netology
 
+fun main(){
+
+    val likes: Int = 11
+    var human: String = if ((likes - 1) % 10 == 0 && (likes != 11 || likes == 0)) "человеку" else "людям"
+
+    println("Понравилось $likes $human")
+}
