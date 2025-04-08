5ea9bdcb63a9d978205727c20438cc3a3f2c0286
Oleg Yakovenko <sc0rpi0n2001@yandex.com>
2025-04-08 22:59:21 +0300

    Исправил var на val

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -3,7 +3,7 @@ package ru.netology
 fun main(){
 
     val likes: Int = 11
-    var human: String = if ((likes - 1) % 10 == 0 && (likes != 11 || likes == 0)) "человеку" else "людям"
+    val human: String = if ((likes - 1) % 10 == 0 && (likes != 11 || likes == 0)) "человеку" else "людям"
 
     println("Понравилось $likes $human")
 }
